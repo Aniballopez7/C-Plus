982863fd404105e9e9f5778540a9af8f0ee59c74
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-01 19:20:03 -0400

    Actualizacion

---
+++ Programador novato/8. Estructuras/Dias de vacaciones.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<ctime>
+#include <clocale>
+using namespace std;
+
+struct Empleados
+{
+    char nombre[50];
+    int aIngresos,aTrabajado,diasV;
+}empleados[3]={{"pepe"},{"maria"},"kuso"};
+
+int main()
+{
+    setlocale(LC_CTYPE,"Spanish");
+    time_t t=time(NULL);
+    tm* timePtr=localtime(&t);
+    int aActual=timePtr->tm_year+1900;
+    cout<<"Ingresa los años de ingreso de cada empleado\n"<<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<"Año de ingreso de "<<empleados[i].nombre<<": ";
+        cin>>empleados[i].aIngresos;
+        empleados[i].aTrabajado = aActual - empleados[i].aIngresos;
+        switch (empleados[i].aTrabajado)
+        {
+        case 0 ... 3: empleados[i].diasV = 5;break;
+        case 4 ... 10: empleados[i].diasV = 10;break;
+        case 11 ... 100: empleados[i].diasV = 15;break;
+        }
+    }
+    cout<<"\nDias de vacaciones por empleados\n"<<endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<"Al empleado "<<empleados[i].nombre<<" le correspondes "<<empleados[i].diasV
+        <<" dias de vacaciones"<<" por sus "<<empleados[i].aTrabajado<<" años tranajados"<<endl;
+    }
+    system("\nread -p '\nPresiona Enter para continuar...' var");
+}
---
+++ Programador novato/8. Estructuras/Notas del alumno.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std; 
+struct Materias
+{
+    char nombre[30];
+    float notas;
+}materias[3]={{"Programacion",0},{"Estructuras de datos",0},{"Matematicas",0}};
+
+int main()
+{
+    float suma=0,promedio;
+    regresar:
+    cout<<"\nIngrese las notas de las materias\n"<<endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<materias[i].nombre<<": ";
+        cin>>materias[i].notas;
+        if ((materias[i].notas<0)||(materias[i].notas>10))
+        {
+            cout<<"\nIngreso una nota invalida..."<<endl;
+            goto regresar;
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        suma += materias[i].notas;
+    }
+    promedio = suma/3;
+    cout<<"\n";
+    if (promedio <= 5.9)
+    {
+        cout<<"No aprobaste el semestre"<<endl;
+    }
+    if (promedio >6)
+    {
+        cout<<"Aprobaste con un promedio de "<<promedio<<endl;
+    }
+    cout<<"\n"<<endl;
+    system("\nread -p '\nPresiona Enter para continuar...' var");
+}
