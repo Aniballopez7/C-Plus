844312a75aa3b3cbc060c71ba2b1477897111f42
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-03 19:16:29 -0400

    Upload

---
+++ Mis programas/Con funciones/Elevar exponente.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<cmath>
+using namespace std; 
+int a,b;
+void pedirDatos();
+int retornarExpo(int a,int b);
+void mostrarExpo(int a,int b);
+//TODO: pedir datos
+void pedirDatos(){
+    regresar:
+    cout<<"Ingrese un numero: ";cin>>a;
+    cout<<"Ingrese el exponente: ";cin>>b;
+    if (a<0)
+    {
+        cout<<"El numero no es positivo"<<endl;
+        cout<<"\n";
+        goto regresar;
+    }
+    if (b<0)
+    {
+        cout<<"El numero no es positivo"<<endl;
+        cout<<"\n";
+        goto regresar;
+    }
+}
+//TODO: retornar
+int retornarExpo(int a,int b){
+    /*
+    long resultado;
+    resultado = a*b;
+    return resultado;
+    */
+    return pow(a,b);
+}
+//TODO: mostrar
+void mostrarExpo(int a, int b){
+    int resultado = retornarExpo(a,b);
+    cout<<"Resultado: "<<resultado<<endl;
+}
+//TODO: Funcion principal
+int main()
+{
+    int resultado;
+    pedirDatos();
+    resultado = retornarExpo(a,b);
+    cout<<"El resultado es: "<<resultado<<endl;
+    mostrarExpo(a,b);
+    system("\nread -p '\nPresiona Enter para continuar...' var");
+}
