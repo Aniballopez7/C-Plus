03d1c6bd3c4232e3f2cf27ad1ff1612b3553b3f7
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-04 18:22:36 -0400

    Master

--- Mis programas/Con funciones/Dinero a dar por un cajero.cpp
+++ Mis programas/Con funciones/Dinero a dar por un cajero.cpp
@@ -6,15 +6,6 @@ int main()
     int dinero,quinientos,doscientos,cien,cincuenta,veinte,diez,cinco,uno;
     cout<<"Ingrese cuanto dinero necesita: ";cin>>dinero;
     Entregar(dinero,quinientos=0,doscientos=0,cien=0,cincuenta=0,veinte=0,diez=0,cinco=0,uno=0);
-    cout<<"\nAqui esta tu dinero:\n"<<endl;
-    cout<<"Quinientos: "<<quinientos<<endl;
-    cout<<"Doscientos: "<<doscientos<<endl;
-    cout<<"Cien: "<<cien<<endl;
-    cout<<"Cincuenta: "<<cincuenta<<endl;
-    cout<<"Veinte: "<<veinte<<endl;
-    cout<<"Diez: "<<diez<<endl;
-    cout<<"Cinco: "<<cinco<<endl;
-    cout<<"Uno: "<<uno<<endl;
     system("\nread -p '\nPresiona Enter para continuar...' var");
     system("clear");
     cout<<"**********************"<<endl;
