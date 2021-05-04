b0f75fcdc481c09abb979b63e04a0e222784f6c5
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-03 20:32:05 -0400

    Upload

--- Mis programas/Con funciones/Parametros por referencia.cpp
+++ Mis programas/Con funciones/Parametros por referencia.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 
 void cambio(int&,int&);
-
+void cambio1(int&,int&);
 int main()
 {
     int n1,n2;
@@ -11,14 +11,22 @@ int main()
     cout<<"\nValores ingresados:\n"<<endl;
     cout<<n1<<endl;
     cout<<n2<<endl;
-    cambio(n1,n2);
     cout<<"\nValores cambiados:\n"<<endl;
+    cambio1(n1,n2);
+    cout<<n1<<endl;
+    cout<<n2<<endl;
+    cout<<"\nValores cambiados manualmente:\n"<<endl;
+    cambio(n1,n2);
     cout<<n1<<endl;
     cout<<n2<<endl;
     system("\nread -p '\nPresiona Enter para continuar...' var");
 }
-
 void cambio(int& n1,int& n2){
+    n1 = 60;
+    n2 = 30;
+}
+
+void cambio1(int& n1,int& n2){
     int aux;
     aux = n1;
     n1 = n2;
