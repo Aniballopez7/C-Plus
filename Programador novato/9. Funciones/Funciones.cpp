9c6d8758ff7cf5c1732b278155c17af6e3b1e2db
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-03 22:54:06 -0400

    Upload

---
+++ Programador novato/9. Funciones/Funciones.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std; 
+
+int suma(int a, int b);
+
+int main()
+{
+    int resultado,valorA,valorB;
+    cout<<"Ingresa el valor de A: ";cin>>valorA;
+    cout<<"Ingrese el valor de B: ";cin>>valorB;
+    resultado = suma(valorA,valorB);
+    cout<<"Resultado: "<<resultado<<endl;
+    system("\nread -p '\nPresiona Enter para continuar...' var");
+}
+
+int suma(int valA, int valB){
+    int resultado;
+    resultado = valA +valB;
+    return resultado;
+}
