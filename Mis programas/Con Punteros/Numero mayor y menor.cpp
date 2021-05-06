5beb0a954db8aab2d5284ab1da41bb3ad53675fd
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-05 23:12:09 -0400

    Master

---
+++ Mis programas/Con Punteros/Numero mayor y menor.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+    int nelementos,numeros[nelementos],*arreglo,menor=999999,mayor=-999999;
+    do
+    {
+        cout<<"Cuantos elementos va a ingresar: ";cin>>nelementos;
+        if (nelementos < 1)
+        {
+            cout<<"Debe ser mayor a 1"<<endl;
+        }
+    } while (nelementos<1);
+    for (int i = 0; i < nelementos; i++)
+    {
+        cout<<"Ingresa un numero "<<i+1<<": ";cin>>numeros[i];
+    }
+    arreglo = numeros;
+    cout<<"\n";
+    for (int i = 0; i < nelementos; i++)
+    {
+        if (*arreglo < menor)
+        {
+            menor = *arreglo;
+        }
+        if (*arreglo > mayor)
+        {
+            mayor = *arreglo;
+        }
+        arreglo++;
+    }
+    cout<<"El numero menor es: "<<menor<<endl;
+    cout<<"El numero mayor es: "<<mayor<<endl;
+    system("\nread -p '\nPresiona Enter para continuar...' var");
+}
