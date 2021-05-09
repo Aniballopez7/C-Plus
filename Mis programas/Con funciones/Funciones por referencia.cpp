ff2948bc72584e53348630c07577f1471632a2fa
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-09 14:15:01 -0400

    Master

--- Mis programas/Con funciones/Funciones por referencia.cpp
+++ Mis programas/Con funciones/Funciones por referencia.cpp
@@ -1,29 +1,24 @@
 #include<iostream>
 using namespace std; 
-void insertarNumero(int n); //(int& n) o (int &n) <-parametros por referecia
-void sumar(int n);
-void mostrar(int n);
+void insertarNumero(int& n); //(int& n) o (int &n) <-parametros por referecia
+void sumarReferencia(int& n);
 /*TODO: el parametro por referencia lo que hace es guardar el valor de un parametro para ser
-utilizado en otra funcion al utilizar ese parametro
+utilizado en otra funcion al utilizar ese parametro.
 */
 int main()
 {
-    int n;
+    int n,b;
     insertarNumero(n);
-    sumar(n);
+    sumarReferencia(n);
     system("\nread -p '\nPresiona Enter para continuar...' var");
 }
-void insertarNumero(int n)
+void insertarNumero(int& n)
 {
     cout<<"Ingresa un numero: ";cin>>n;
     cout<<n<<endl;
 }
-void sumar(int n)
+void sumarReferencia(int& n)
 {
     n += 5;
     cout<<n<<endl;
-}
-void mostrar(int n)
-{
-
 }
