e7923fdd1d7bf2a8b0d407aeb51d6e63c922af9e
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-11 00:27:45 -0400

    Master

--- Uni/Program III/Arreglo 10.cpp
+++ Uni/Program III/Arreglo 10.cpp
@@ -1,8 +1,9 @@
 /*
 Llene un arreglo con 10 números , calcule el promedio de los elementos y cuantos elementos 
-están por encima del promedio. Realice la mismas operación para el Vector B = 4A. muestre 
-el promedio de A y de B, indique cual es el mayor de los promedios. Ordene con el método de 
-la burbuja el vector a y muéstrelo.
+están por encima del promedio. Realice la mismas operación para el Vector B = 
+
+4 muestre el promedio de A y de B, indique cual es el mayor de los promedios. Ordene con el 
+método de la burbuja el vector a y muéstrelo.
 */
 
 #include<iostream>
@@ -10,6 +11,7 @@ using namespace std;
 int main()
 {
     int vecA[10],vecB[10],sumaA=0,sumaB=0,auxA,promedioA,promedioB;
+    int encima_promedioA=0,encima_promedioB=0;
     bool ordenado=false;
     cout<<"Vector A:\n"<<endl;
     for (int i = 0; i < 10; i++)
@@ -28,6 +30,21 @@ int main()
     promedioB = sumaB/10;
     cout<<"\nPromedio A: "<<promedioA<<endl;
     cout<<"Promedio B: "<<promedioB<<endl;
+    //Contando elementos por encima del promedio
+    for (int i = 0; i < 10; i++)
+    {
+        if (vecA[i]>promedioA)
+        {
+            encima_promedioA++;
+        }
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        if (vecA[i]>promedioB)
+        {
+            encima_promedioB++;
+        }
+    }
     if (promedioA>promedioB)
     {
         cout<<"El promedio del vector A es mayor al promedio del vector B"<<endl;
@@ -36,9 +53,9 @@ int main()
     {
         cout<<"El promedio del vector B es mayor al promedio del vector A"<<endl;
     }
-
+    cout<<"Hay "<<encima_promedioA<<" elementos que estan por encima del promedio del vector A"<<endl;
+    cout<<"Hay "<<encima_promedioB<<" elementos que estan por encima del promedio del vector B"<<endl;
     cout<<"\nVector A sin ordenar:\n"<<endl;
-
     for (int i = 0; i < 5; i++)
     {
         cout<<vecA[i]<<endl;
---
+++ Uni/Program III/Modificar.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int n,i,x[100],salto,tem,band;
+    char s;
+
+        cout<<"defina la cantidad de elementos:";
+        cin>>n;
+        for(i=0;i<n;i++)
+        {
+            cout<<"elemento["<<i<<"]:";
+            cin>>x[i];
+        }
+        cout<<"\nElementos sin ordenar:\n"<<endl;
+        for(i=0;i<n;i++)
+        {
+            cout<<x[i]<<" ";
+            cout<<endl;
+        }
+        for(salto=n/2;salto>0;salto=salto/2)
+        {
+            do
+            {
+                band=0;
+                for(i=0;i<n-salto;i++)
+                {
+                    if(x[i]>x[i+salto])
+                    {
+                    tem=x[i];
+                    x[i]=x[i+salto];
+                    x[i+salto]=tem; 
+                    band=1;
+                    }
+                }
+                
+            }while(band==0);
+        }
+        cout<<"\nElementos ordenados:\n"<<endl;
+        for(i=0;i<n;i++)
+        {
+            cout<<x[i]<<" ";
+            cout<<endl;
+        }
+    getch();
+    return 0;
+}
