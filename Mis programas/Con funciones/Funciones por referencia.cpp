de7020bb4db04be374cca9ed645cd9e7b7379131
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-16 22:12:57 -0400

    Master

--- Mis programas/Con funciones/Funciones por referencia.cpp
+++ Mis programas/Con funciones/Funciones por referencia.cpp
@@ -1,24 +1,37 @@
 #include<iostream>
 using namespace std; 
-void insertarNumero(int n); //(int& n) o (int &n) <-parametros por referecia
-void sumarReferencia(int n);
+void insertarNumero(int& n,int& b); //(int& n) o (int &n) <-parametros por referecia
+void sumarReferencia(int& n, int b); //La b no esta por parametros por referencia
+void mostrar(int n, int b); //No tienen parametros por referencia
 /*TODO: el parametro por referencia lo que hace es guardar el valor de un parametro para ser
 utilizado en otra funcion al utilizar ese parametro.
+
+Ejemplo si tu declaras un parametro por referencia -> (int& numero)
+y en la varieble numero le asignas el 5, esa variable conservara el 
+mismo valor de 5 cuando utilices el parametro (int numero) en 
+otra funcion.
+
 */
 int main()
 {
-    int n;
-    insertarNumero(n);
-    sumarReferencia(n);
+    int n,b;
+    insertarNumero(n,b);
+    sumarReferencia(n,b);
+    mostrar(n,b);
     system("\nread -p '\nPresiona Enter para continuar...' var");
 }
-void insertarNumero(int n)
+void insertarNumero(int& n,int& b)
 {
     cout<<"Ingresa un numero: ";cin>>n;
-    cout<<"\n"<<n<<endl;
+    b = n;
 }
-void sumarReferencia(int n)
+void sumarReferencia(int& n,int b)
 {
     n += 5;
-    cout<<n<<endl;
+    b += 5;
+}
+void mostrar(int n, int b)
+{
+    cout<<"\nCon referencia: "<<n<<endl;
+    cout<<"Sin referencia: "<<b<<endl;
 }
