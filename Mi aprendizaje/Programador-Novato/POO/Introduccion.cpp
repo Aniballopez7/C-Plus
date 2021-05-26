70844c85dda72a0ce5b4f5ba04673de908ebe88d
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-26 12:54:17 -0400

    Master

---
+++ Mi aprendizaje/Programador-Novato/POO/Introduccion.cpp
@@ -0,0 +1,43 @@
+/*
+La programación Orientada a objetos se define como un paradigma de la programación, una manera de programar 
+específica, donde se organiza el código en unidades denominadas clases, de las cuales se crean objetos que 
+se relacionan entre sí para conseguir los objetivos de las aplicaciones.
+
+La programación Orientada a objetos (POO) es una forma especial de programar, más cercana a cómo expresaríamos 
+las cosas en la vida real que otros tipos de programación.
+
+Pensar en términos de objetos es muy parecido a cómo lo haríamos en la vida real. Por ejemplo vamos a pensar en 
+un coche para tratar de modelizarlo en un esquema de POO. Diríamos que el coche es el elemento principal que 
+tiene una serie de características, como podrían ser el color, el modelo o la marca. Además tiene una serie 
+de funcionalidades asociadas, como pueden ser  marchar, parar o aparcar.
+
+Una clase es una abstracción de un objeto, retomando el ejemplo del auto, es una clase de auto que tiene 
+características de ahora en adelante atributos y también funcionalidades de ahora en adelante métodos.
+
+Conceptos a estudiar de la POO
+- Clase.
+- Abstracción.
+- Encapsulamiento.
+- Herencia.
+- Polimorfismo.
+
+- Abstracción.
+Una abstracción se enfoca en la visión externa de un objeto,  separa el comportamiento  específico de un objeto, 
+a esta división que realiza se le conoce como la barrera de abstracción, la cual se consigue aplicando el principio 
+de mínimo compromiso.
+
+- Encapsulamiento.
+Es un proceso en el que se ocultan los detalles del soporte de las características de una abstracción.
+
+- Herencia.
+Es el mecanismo por el cual una clase permite heredar las características (atributos y métodos) de otra clase.
+
+- Polimorfismo.
+El polimorfismo es la cualidad que poseen los objetos para responder de distinto modo ante el mismo mensaje.
+
+TODO: resumen:
+
+Las clases es el o los objetos.
+Los atributos son sus caracteristicas.
+Los metodos son sus funcionalidades o lo que hace.
+*/
