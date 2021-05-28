54b7863778330ecd04f4bbdf74586e4bdb9dd642
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-28 18:43:27 -0400

    Master

---
+++ Inicio con menu.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <clocale>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE,"Spanish");
+    int opc;
+    char r;
+    bool salir = false;
+    do
+    {
+        system("clear");
+        cout<<"1. Ingresar elemento"<<endl;
+        cout<<"2. "<<endl;
+        cout<<"3. "<<endl;
+        cout<<"4. "<<endl;
+        cout<<"5. "<<endl;
+        cout<<"6. Salir\n"<<endl;
+        cout<<"Ingresar una opcion: ";cin>>opc;
+        switch (opc)
+        {
+            case 1:
+                {
+                    system("clear");
+                    system("\nread -p '\nPresiona Enter para continuar...' var");
+                }break;
+            case 2:
+                {
+                    system("clear");
+                    system("\nread -p '\nPresiona Enter para continuar...' var");
+                }break;
+            case 3:
+                {
+                    system("clear");
+                    system("\nread -p '\nPresiona Enter para continuar...' var");
+                }break;
+            case 4:
+                {
+                    system("clear");
+                    system("\nread -p '\nPresiona Enter para continuar...' var");
+                }break;
+            case 5:
+                {
+                    salir = true;
+                }break;
+        }
+    } while (salir != true);
+    system("\nread -p '\nPresiona Enter para volver al menu...' var");
+}
