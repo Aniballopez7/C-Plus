3d118a4ea7a3b8160ecaa208d02066bfd8059253
Aniballopez7 <aniballopez0708@gmail.com>
2021-05-28 19:29:19 -0400

    Master

--- Inicio con menu.cpp
+++ Inicio con menu.cpp
@@ -10,34 +10,31 @@ int main()
     do
     {
         system("clear");
-        cout<<"1. Ingresar elemento"<<endl;
+        cout<<"1. "<<endl;
         cout<<"2. "<<endl;
         cout<<"3. "<<endl;
         cout<<"4. "<<endl;
         cout<<"5. "<<endl;
         cout<<"6. Salir\n"<<endl;
         cout<<"Ingresar una opcion: ";cin>>opc;
+        system("clear");
         switch (opc)
         {
             case 1:
                 {
                     system("\nread -p '\nPresiona Enter para continuar...' var");
-                    system("clear");
                 }break;
             case 2:
                 {
                     system("\nread -p '\nPresiona Enter para continuar...' var");
-                    system("clear");
                 }break;
             case 3:
                 {
                     system("\nread -p '\nPresiona Enter para continuar...' var");
-                    system("clear");
                 }break;
             case 4:
                 {
                     system("\nread -p '\nPresiona Enter para continuar...' var");
-                    system("clear");
                 }break;
             case 5:
                 {
