fef2ccbbaa6a0ac53d005ac76183dc0e6541a342
Aniballopez7 <aniballopez0708@gmail.com>
2021-06-01 17:56:58 -0400

    Master

--- Uni/Program I/3 en 1.cpp
+++ Uni/Program I/3 en 1.cpp
@@ -6,63 +6,73 @@ Numero mayor*/
 #include<iostream>
 #include<conio.h>
 using namespace std;
-int main(){
-	int opc,numero,inicio;
+int main()
+{
+	int opc,numero;
 	char letra;
 	float n1,n2;
-	for(inicio=0;inicio>=0;inicio++){
-	cout<<"1. Programa de vocales"<<endl;
-	cout<<"2. Programa de numero par o impar"<<endl;
-	cout<<"3. Programa de numero mayor"<<endl;
-	cout<<"0. Salir"<<endl;
-	do{
+	do
+	{
+		system("clear");
+		cout<<"1. Programa de vocales"<<endl;
+		cout<<"2. Programa de numero par o impar"<<endl;
+		cout<<"3. Programa de numero mayor"<<endl;
+		cout<<"0. Salir"<<endl;
 		cout<<"\nOpcion: ";cin>>opc;
-	}while(opc>3);
-	system("cls");
-	switch(opc){
+	}while(opc > 3);
+	system("clear");
+	switch(opc)
+	{
 		case 0:
 			return 0;
 		case 1:
-		cout<<"Ingrese una letra: ";cin>>letra;
-		switch(letra){
-		case 'a':
-		case 'e':
-		case 'i':
-		case 'o':
-		case 'u': cout<<"\nEs una vocal minuscula"<<endl;break;
-		default: cout<<"\nNo es una vocal"<<endl;break;
-		cout<<"\n";
-		}
-		getch();
-	    system("cls");
-		break;
-	case 2:
-		cout<<"Ingrese un numero: ";cin>>numero;
-	if(numero==0){
-		cout<<"\nEl numero es cero"<<endl;
-	}else if(numero%2==0){
-		cout<<"\nEl numero es par"<<endl;	
-	}else if(numero%2!=0){
-		cout<<"\nEl numero es impar"<<endl;
+			cout<<"Ingrese una letra: ";cin>>letra;
+			switch(letra)
+			{
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u': cout<<"\nEs una vocal minuscula"<<endl;break;
+			default: cout<<"\nNo es una vocal"<<endl;break;
+			cout<<"\n";
+			}
+			getch();
+			break;
+		case 2:
+				cout<<"Ingrese un numero: ";cin>>numero;
+			if(numero == 0)
+			{
+				cout<<"\nEl numero es cero"<<endl;
+			}
+			else if((numero % 2) == 0)
+			{
+				cout<<"\nEl numero es par"<<endl;	
+			}
+			else if((numero % 2) != 0)
+			{
+				cout<<"\nEl numero es impar"<<endl;
+			}
+			getch();
+			break;
+		case 3:
+			cout<<"Ingrese un numero: ";cin>>n1;
+			cout<<"\nIngrese otro numero: ";cin>>n2;
+			if(n1 == n2)
+			{
+				cout<<"\nLos numeros son iguales"<<endl;	
+			}
+			else if(n1 > n2)
+			{
+				cout<<"\nEl mayor es: "<<n1<<endl;	
+			}
+			else
+			{
+				cout<<"\nEl mayor es: "<<n2<<endl;
+			}
+			getch();
+			break;
 	}
-	getch();
-	system("cls");
-	break;
-	case 3:
-		cout<<"Ingrese un numero: ";cin>>n1;
-	cout<<"\nIngrese otro numero: ";cin>>n2;
-	if(n1==n2){
-		cout<<"\nLos numeros son iguales"<<endl;	
-	}else if(n1>n2){
-		cout<<"\nEl mayor es: "<<n1<<endl;	
-    }else{
-    	cout<<"\nEl mayor es: "<<n2<<endl;
-	}
-	getch();
-	system("cls");
-	break;
-	}
-}
 	cout<<"\n";
 	getch();
 	return 0;
--- Uni/Program I/Dias de la semana.cpp
+++ Uni/Program I/Dias de la semana.cpp
@@ -3,23 +3,36 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-int main(){
+int main()
+{
 	int numero;
 	cout<<"Ingrese un numero del 1 al 7: ";cin>>numero; 
 	
-	if(numero==1){
+	if(numero == 1)
+	{
 		cout<<"\nEl dia es Domingo";
-	}else if(numero==2){
+	}
+	else if(numero == 2)
+	{
 		cout<<"\nEl dia es Lunes";
-	}else if(numero==3){
+	}
+	else if(numero == 3)
+	{
 		cout<<"\nEl dia es Martes";
-	}else if(numero==4){
+	}
+	else if(numero == 4)
+	{
 		cout<<"\nEl dia es Miercoles";
-	}else if(numero==5){
+	}
+	else if(numero == 5)
+	{
 		cout<<"\nEl dia es Jueves";
-	}else if(numero==6){
+	}
+	else if(numero == 6)
+	{
 		cout<<"\nEl dia es Viernes";
-	}else if(numero==7){
+	}
+	else if(numero==7){
 		cout<<"\nEl dia es Sabado";
 	}
 	cout<<"\n";
